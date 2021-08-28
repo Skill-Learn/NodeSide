3e9be5d30deb54b60477898aa1da394c97088025
Elias Melaku <se.elias.melaku@gmail.com>
2021-08-28 18:48:52 +0300

    setting up auth

--- API/routes/auth.js
+++ API/routes/auth.js
@@ -2,7 +2,10 @@ const router = require('express').Router();
 const User = require('../models/User');
 
 router.post('/register', (req, res) => {
-
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
 });
 
 module.exports = router;
